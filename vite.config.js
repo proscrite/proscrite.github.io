bff46b0af7d2cfaf5522aff6eb17f878c2d9b98c
proscrite <pablogfr94@gmail.com>
2025-08-03 14:12:04 +0300

    fix: update project name in package.json and ensure correct homepage URL

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
   base: '/',
+  plugins: [react()],
   build: {
     assetsInlineLimit: 0, // Don't inline any assets as base64
     assetsDir: 'assets', // Keep assets in the assets directory

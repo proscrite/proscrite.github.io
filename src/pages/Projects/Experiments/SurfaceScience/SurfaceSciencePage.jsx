5223c29bc2cd05f71a21a2c8815025413c1837d9
proscrite <pablogfr94@gmail.com>
2026-02-05 10:50:23 +0200

    Added "planting molecules" animation in SurfaceScience index.

--- src/pages/Projects/Experiments/SurfaceScience/SurfaceSciencePage.jsx
+++ src/pages/Projects/Experiments/SurfaceScience/SurfaceSciencePage.jsx
@@ -3,6 +3,33 @@ import { Link, useNavigate } from 'react-router-dom'
 import { HashLink } from 'react-router-hash-link'
 import './SurfaceScience.css'
 import './SurfaceTechniques.css'
+import ImageCarousel from '../../../../components/ImageCarousel'
+
+
+import moleculeCrop1 from './images/molecule_preparation/molecule_preparation.001.png'
+import moleculeCrop2 from './images/molecule_preparation/molecule_preparation.002.png'
+import moleculeCrop3 from './images/molecule_preparation/molecule_preparation.003.png'
+import moleculeCrop4 from './images/molecule_preparation/molecule_preparation.004.png'
+import moleculeCrop5 from './images/molecule_preparation/molecule_preparation.005.png'
+import moleculeCrop6 from './images/molecule_preparation/molecule_preparation.006.png'
+import moleculeCrop7 from './images/molecule_preparation/molecule_preparation.007.png'
+import moleculeCrop8 from './images/molecule_preparation/molecule_preparation.008.png'
+import moleculeCrop9 from './images/molecule_preparation/molecule_preparation.009.png'
+import moleculeCrop10 from './images/molecule_preparation/molecule_preparation.010.png'
+const moleculeCrops = [moleculeCrop1, moleculeCrop2, moleculeCrop3, moleculeCrop4, moleculeCrop5, 
+  moleculeCrop6, moleculeCrop7, moleculeCrop8, moleculeCrop9, moleculeCrop10];
+const moleculeCaptions = [
+  'The field at first is rugged and heterogeneous, and it needs to be flatten and prepared for the molecules to be sown.',
+  'The first step is annealing, a term used by smiths, which consists on heating the substrate to high temperatures to remove contaminants and smooth the surface. This step would actually be the opposite of plowing, as the goal is to create a smooth and clean surface.',
+  'Once the field is smooth, it can still contain some stubborn pebbles, stones and weeds (contaminants) that need to be removed. ',
+  'This is done by sputtering, which is like using a strong herbicide to eliminate unwanted plants from the field.',
+  'Now the field is ready for sowing the seeds (molecules).',
+  'There are different methods to sow the seeds, with the objective of distributing them evenly across the field. ',
+  'When the seeds are sown, we can start experimenting with them, for example by watering them with different solutions to see how they grow.',
+  'We can also use fertilizers (like adding Barium) to enhance their growth and performance.',
+  'Here we see different plants at different stages of growth. ',
+  'Finally, we can harvest the crops, which in our case means collecting the fluorescent signal emitted by the molecules when they capture Ba²⁺ ions.',
+];
 
 import preparationImg from './images/preparation.png'
 import characterizationImg from './images/characterization.png'
@@ -17,7 +44,9 @@ const areas = [
   target: '/projects/experiments/SurfaceScience/results' },
 ];
 
+
 export default function SurfaceSciencePage() {
+  const [currentCaptionIndex, setCurrentCaptionIndex] = React.useState(0);
   const navigate = useNavigate();
   const handleClick = (target) => {
     if (!target) return;
@@ -35,8 +64,13 @@ export default function SurfaceSciencePage() {
       <p align='justify'>
         The BOLD project revolves around developing a novel sensor technology based on molecules that can capture Ba²⁺ ions and emit a fluorescence signal.
         These molecules need to be immobilized on a substrate surface in a controlled manner to ensure optimal performance of the sensor.
-        Therefore, surface science techniques are employed to prepare and analyze these functionalized surfaces.
+        You can think of this process as a sort of "sowing seeds" on a surface, where the seeds are the molecules and the soil is the substrate. 
+        Just like in gardening, the way you prepare the soil and plant the seeds can greatly affect how well they grow and function. 
+        The following animation shows a simplified representation of the process of surface functionalization. Click on the image to cycle through the steps.
       </p>
+      <div className="image-carousel-container">
+        <ImageCarousel images={moleculeCrops} captions={moleculeCaptions} width={100}/>
+      </div>
       
       <p align='justify'>
         Characterization of the functionalized surfaces is essential to verify the survival of molecules to the deposition processes, their successful immobilization on surfaces and to assess the properties of the prepared sample.
